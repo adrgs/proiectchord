21a84e261b7363cffbc3de36ebeff621882e99ac
adrgs <albastroiudragos@gmail.com>
2022-01-18 01:10:12 +0200

    Fix protobuf gen

--- genpb.sh
+++ genpb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative chordpb/chordpb.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. chordpb/chordpb.proto
